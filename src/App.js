7811272342a5ea804f4b2c1fe9c5039144062656
TheEmerald04 <fredcharles.fred.charles@gmail.com>
2025-10-26 21:41:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React, { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
-import { FaReact } from 'react-icons/fa';
 import './App.css';
-
-const components = {
-  p: ({node, ...props }) => {
-    if (props.children && typeof props.children[0] === 'string' && props.children[0].includes('::react-icons::')) {
-      return <FaReact />; 
-    }
-    return <p {...props} />;
-  },
-};
 
 function App() {
   const [markdown, setMarkdown] = useState('# Markdown Preview');
